11d2383903b5cb68dd00f0405eb84127e112f377
kummitisowmya <sowmyakummiti@gmail.com>
2025-03-16 01:30:06 +0530

    i have added code

---
+++ sales/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from sales.views import *
+
+urlpatterns = [
+    path('admin-dashboard/', admin_dashboard, name='admin_dashboard'),
+    path('home/', sales_home, name='sales_home'),
+    path('dashboard/', sales_dashboard, name='sales_dashboard'),
+    path('enroll-student/', enroll_student, name='enroll_student'),
+    path('record-payment/<int:student_id>/', record_payment, name='record_payment'),
+    path('pending-payments/', view_pending_payments, name='view_pending_payments'),
+    path('create-pending-payment/<int:student_id>/', create_pending_payment, name='create_pending_payment'),
+    path('add-user/', add_user, name='add_user'),
+    path('add-class/', add_class, name='add_class'),
+]
