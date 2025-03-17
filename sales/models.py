683c5b2f21ec39ba9d7a54d95eccdc34ac85ed9d
kummitisowmya <sowmyakummiti@gmail.com>
2025-03-17 16:54:24 +0530

    Update models.py

--- sales/models.py
+++ sales/models.py
@@ -20,19 +20,6 @@ class TrainingClass(models.Model):
     def __str__(self):
         return self.name
 
-# Student Model
-# class Student(models.Model):
-#     name = models.CharField(max_length=255)
-#     email = models.EmailField(unique=True)
-#     phone = models.CharField(max_length=15)
-#     joined_date = models.DateField(auto_now_add=True)
-#     training_class = models.ForeignKey(TrainingClass, on_delete=models.CASCADE)
-#     salesperson = models.ForeignKey(SalesPerson, on_delete=models.SET_NULL, null=True, blank=True)
-#     total_fees = models.DecimalField(max_digits=10, decimal_places=2, default=0)  # ✅ Add this field
-#     payment_status = models.CharField(max_length=20, default='not_updated')  # Add this field
-#     is_enrolled = models.BooleanField(default=True)
-#     def __str__(self):
-#         return self.name
 from django.db import models
 
 class Student(models.Model):
